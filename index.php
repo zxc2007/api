2913650fa2048a59ea1227cc0c2905345399b4f2
Hen-Dricks <62672799+Hen-Dricks@users.noreply.github.com>
2020-09-08 22:44:06 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-// $u = 'http://api.local/index.php?id=1&to=arkosmillers21@gmail.com&x='.urlencode('blog.mikabenjamin.com|587|smtpfox-m7uib@blog.mikabenjamin.com|Fox#V1LXQOlyeqY#Xo');
-
-// echo $u;
-// exit();
-
-//http://api.local/index.php?id=1&to=arkosmillers21@gmail.com&x=blog.mikabenjamin.com%7C587%7Csmtpfox-m7uib%40blog.mikabenjamin.com%7CFox%23V1LXQOlyeqY%23Xo
 
 require 'vendor/autoload.php';
 
@@ -55,5 +49,5 @@ use PHPMailer\PHPMailer\SMTP;
         $r = smtest($id, $host, $user, $pass, $port, $from, $to);
         echo $r;
     }else{
-        echo json_encode('Invalid', JSON_PRETTY_PRINT);
+        echo 'Invalid';
     }
